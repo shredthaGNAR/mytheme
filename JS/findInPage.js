deecbd71dabe019ff3f222e27c01eff7351342ff
lukiz <wynstaxxx@gmail.com>
2021-11-08 01:04:56 -0800

    fixed good

--- JS/findInPage.js
+++ JS/findInPage.js
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* import-globals-from extensionControlled.js */
-/* import-globals-from preferences.js */
+// import-globals-from extensionControlled.js
+// import-globals-from preferences.js
 
 // A tweak to the standard <button> CE to use textContent on the <label>
 // inside the button, which allows the text to be highlighted when the user
